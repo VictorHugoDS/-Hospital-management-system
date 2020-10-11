42958c5fc9d048f3732c85ffd1de0f75c42a5ce5
victor hugo <victorhugod.s@hotmail.com>
2020-10-11 10:45:41 -0400

    Edições adicionais

--- src/hospital/management/system/entidades/Consulta.java
+++ src/hospital/management/system/entidades/Consulta.java
@@ -1,6 +1,6 @@
 package hospital.management.system.entidades;
 
-<<<<<<< HEAD
+
 public class Consulta extends Base {
     private String relatorio_do_paciente;
     private String tratamento;
@@ -8,21 +8,5 @@ public class Consulta extends Base {
     private Medico medico;
     private Paciente paciente;
     private Consultorio consultorio;
-=======
-public class Consulta {
-    private String relatorio;
-    private String tratamento;
-    private Medico medico;
-    private Paciente paciente;
->>>>>>> 581f525e8033f6ec0893d8ee15e715a12ab530e8
-
-
-    public String getTratamento() {
-        return tratamento;
-    }
-
-    public void setTratamento(String tratamento) {
-        this.tratamento = tratamento;
-    }
 
 }
